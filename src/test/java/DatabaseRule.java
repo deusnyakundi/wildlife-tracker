0470c3f52e1f6f31593069bb74bf262847146160
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 17:31:18 +0300

    Catch unable to connect to DB exception

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,18 +5,23 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DatabaseRule extends ExternalResource {
-    @Rule
-    public DatabaseRule databaseRule = new DatabaseRule();
     @Override
     protected void before() throws Throwable {
-        DB.sql2o = new Sql2o("jdbc://localhost:5432/wild_life_test", "postgres", "superadmin");
+        try {
+            DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "Access");
+        }
+        catch (Exception e){
+            System.out.println("Unable to connect to Database. Check your connection string");
+        }
     }
 
     @Override
     protected void after() {
         try (Connection con = DB.sql2o.open()) {
-            String deleteSightingsQuery = "DELETE FROM sightings *";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
             con.createQuery(deleteSightingsQuery).executeUpdate();
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
         }
     }
 }
