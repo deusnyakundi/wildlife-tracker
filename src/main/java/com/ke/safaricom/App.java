3a071a9318850b0d92c86086dbf781f49be9c7df
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 16:06:24 +0300

    Process user inputs from sightings form

--- src/main/java/com/ke/safaricom/App.java
+++ src/main/java/com/ke/safaricom/App.java
@@ -3,11 +3,14 @@ package com.ke.safaricom;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -30,6 +33,23 @@ public class App {
 
             return new ModelAndView(model, "sightings-all.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/sightings/add", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String wildLifeName = request.queryParams("wildlifeName");
+            Wildlife wildlife = (Wildlife) Wildlife.findByName(wildLifeName);
+            int wildlifeId = wildlife.getId();
+            String zone = request.queryParams("location");
+            String rangerName = request.queryParams("rangerName");
+            String timeString = request.queryParams("time");
+            Timestamp time = new Timestamp(new Date(timeString).getTime());
+
+            Sighting sighting = new Sighting(wildlifeId, zone, rangerName, time);
+            sighting.save();
+
+            response.redirect("/");
+
+        });
 
     }
 }
---
+++ src/main/java/com/ke/safaricom/interfaces/Wildlife.java
@@ -0,0 +1,11 @@
+package com.ke.safaricom.interfaces;
+
+public class Wildlife {
+    Object findByName(String wildLifeName);
+
+    int getId();
+
+    String getName();
+    void setId();
+    void setName();
+}
