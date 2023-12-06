7fd9668aab73059bd3bf6bb148cc03c63e37ab0f
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 14:23:31 +0300

    Test that Sighting instatitates with zone

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,1 +1,46 @@
 
+import org.junit.jupiter.api.Test;}
+
+@Test
+
+public void SightingInstantiatesWithWildlifeId_True() {
+
+        Sighting testSighting = setUpNewSighting();
+
+        int testWildlifeId = 1;
+
+        assertEquals(testWildlifeId, testSighting.getWildlifeId());
+
+        }
+
+
+@Test
+
+public void SightingInstantiatesWithSightingZone_True() {
+
+        Sighting testSighting = setUpNewSighting();
+
+        String testSightingZone = "Zone A";
+
+        assertEquals(testSightingZone,testSighting.getSightingZone());
+
+        }
+
+
+
+
+private Sighting setUpNewSighting() {
+
+@@ -32,6 +38,4 @@ private Sighting setUpNewSighting() {
+
+        }
+
+
+
+
+
+
+
+
+
+        }
