026ea74b2d8e8ca6e728c4295d13c99ad23e18e4
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 17:03:43 +0300

    Implement and confirm endagered can be retrieved from DB

--- src/main/java/com/ke/safaricom/Endangered.java
+++ src/main/java/com/ke/safaricom/Endangered.java
@@ -1,27 +1,20 @@
 package com.ke.safaricom;
-
 import com.ke.safaricom.interfaces.Wildlife;
 import org.sql2o.Connection;
-
 import java.util.List;
 import java.util.Objects;
-
 public class Endagered extends Wildlife {
     private String age;
     public int id;
     private String health;
     public static final String DATABASE_TYPE = "endagered";
     private String name;
-
-
     public Endagered(String age, String health,String name) {
         this.age = age;
         this.health = health;
         type = DATABASE_TYPE;
         this.name=name;
-
     }
-
     public void save() {
         try (Connection con = DB.sql2o.open()) {
             String sql = "INSERT INTO animals (name,type,age,health) VALUES(:name,:type,:age,:health)";
@@ -32,10 +25,9 @@ public class Endagered extends Wildlife {
                     .addParameter("health", this.health)
                     .executeUpdate()
                     .getKey();
-
-
+        }
+//        System.out.println(this.type);
     }
-
     public static List<Endagered> all() {
         String sql = "SELECT * FROM animals WHERE type='endagered';";
         try (Connection con = DB.sql2o.open()) {
@@ -44,7 +36,9 @@ public class Endagered extends Wildlife {
                     .executeAndFetch(Endagered.class);
 
         }
+
     }
+
 
     public static Endagered find(int id) {
         try (Connection con = DB.sql2o.open()) {
@@ -55,27 +49,21 @@ public class Endagered extends Wildlife {
                     .executeAndFetchFirst(Endagered.class);
         }
     }
-
     public String getAge() {
         return age;
     }
-
     public void setAge(String age) {
         this.age = age;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getHealth() {
         return health;
     }
-
     public void setHealth(String health) {
         this.health = health;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -87,17 +75,14 @@ public class Endagered extends Wildlife {
                 Objects.equals(type, endagered.type) &&
                 Objects.equals(name, endagered.name);
     }
-
     @Override
     public String getName() {
         return name;
     }
-
     @Override
     public void setName(String name) {
         this.name = name;
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(age, id, health, type);
--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -7,31 +7,26 @@ import static org.junit.Assert.*;
 public class EndageredTest {
     @Rule
     public DatabaseRule databaseRule = new DatabaseRule();
-
     @Test
     public void EndageredInstantiatesCorrectly_True() {
         Endagered testEndagered = setUpNewEndagered();
         assertTrue(testEndagered instanceof Endagered);
     }
-
     private Endagered setUpNewEndagered() {
         return new Endagered("young", "healthy","lioness");
     }
-
     @Test
     public void testAnimalInstantiatesWithAge_True() {
         Endagered testEndagered = setUpNewEndagered();
         String testEndageredAge = "young";
         assertEquals(testEndageredAge, testEndagered.getAge());
     }
-
     @Test
     public void testAnimalInstantiatesWithName_True() {
         Endagered testEndagered = setUpNewEndagered();
         String testEndageredName = "lioness";
         assertEquals(testEndageredName, testEndagered.getName());
     }
-
     @Test
     public void equalsReturnsTrueIfAttributesAreEqual() {
         Endagered testEndagered = setUpNewEndagered();
@@ -40,10 +35,21 @@ public class EndageredTest {
     }
 
     @Test
-    public void save_insertsObjectIntoDatabase_Animal() {
+    public void save_insertsObjectIntoDatabase_Endagered() {
         Endagered testEndagered = setUpNewEndagered();
         testEndagered.save();
-        System.out.println(testEndagered.getType());
-        System.out.println(Endagered.all().get(0).getType());
         assertEquals(Endagered.all().get(0), testEndagered);
+    }
+
+
+    @Test
+    public void all_returnsAllInstancesOfEndagered_true() {
+        Endagered testEndagered = setUpNewEndagered();
+        testEndagered.save();
+        Endagered testEndageredTwo = setUpNewEndagered();
+
+        testEndageredTwo.save();
+        System.out.println(Endagered.all().size());
+        assertEquals(Endagered.all().get(0), testEndagered);
+        assertEquals(Endagered.all().get(1), testEndageredTwo);
     }
