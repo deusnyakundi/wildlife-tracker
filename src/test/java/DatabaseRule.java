efa6f5144f2dfe7420767e22159ff8866a010eb9
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 15:03:07 +0300

    Inject DB rules into Sighting test class

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,9 +1,12 @@
 import com.ke.safaricom.DB;
+import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DatabaseRule extends ExternalResource {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
     @Override
     protected void before() throws Throwable {
         DB.sql2o = new Sql2o("jdbc://localhost:5432/wildlife_tracker_test", "postgres", "superadmin");
