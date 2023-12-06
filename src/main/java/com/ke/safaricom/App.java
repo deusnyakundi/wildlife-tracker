bfcb6fcc0698d83b7827816787c8ca226f86d448
deusnyakundi <nyakundideus1@gmail.com>
2023-12-06 15:48:33 +0300

    Adjust color scheme in home view

--- src/main/java/com/ke/safaricom/App.java
+++ src/main/java/com/ke/safaricom/App.java
@@ -17,5 +17,11 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/sightings/add", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            return new ModelAndView(model, "animal-add-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
